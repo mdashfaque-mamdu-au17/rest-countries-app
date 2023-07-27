3bc266b9169d5f6e982237132700504cb2a2947d
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-07-27 20:35:30 +0530

    changes in data for regions

--- utils/regions.js
+++ utils/regions.js
@@ -1,7 +1,8 @@
 export const regions = [
-  { id: 1, name: 'Africa' },
-  { id: 2, name: 'America' },
-  { id: 3, name: 'Asia' },
-  { id: 4, name: 'Europe' },
-  { id: 5, name: 'Oceania' },
+  { id: 1, name: 'All' },
+  { id: 2, name: 'Africa' },
+  { id: 3, name: 'America' },
+  { id: 4, name: 'Asia' },
+  { id: 5, name: 'Europe' },
+  { id: 6, name: 'Oceania' },
 ];
