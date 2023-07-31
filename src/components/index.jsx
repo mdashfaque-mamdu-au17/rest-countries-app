b3a2088b6054c116df0ba318a65d31311edebebd
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-07-31 23:32:55 +0530

    component export updated

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,14 @@ import Header from './Header/Header';
 import SearchBar from './SearchBar/SearchBar';
 import CountryCard from './CountryCard/CountryCard';
 import ListBoxSelect from './ListBox/ListBoxSelect';
+import Button from './Button/Button';
+import SubTitle from './CountryCard/SubTitle';
 
-export { Header, SearchBar, CountryCard, ListBoxSelect };
+export {
+  Header,
+  SearchBar,
+  CountryCard,
+  ListBoxSelect,
+  Button,
+  SubTitle,
+};
