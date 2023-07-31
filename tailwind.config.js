4fb3da0a51c3375991547bbb915d26cdb0c76c7f
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-07-31 23:32:27 +0530

    new shadow added

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
         'nav-shadow': '0px 2px 4px 0px rgba(0, 0, 0, 0.06)',
         'search-shadow': '0px 2px 9px 0px rgba(0, 0, 0, 0.05)',
         'card-shadow': '0px 0px 7px 2px rgba(0, 0, 0, 0.03)',
+        'button-shadow': '0px 0px 7px 0px rgba(0, 0, 0, 0.29)',
       },
     },
   },
