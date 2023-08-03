394b08893041d9a61a389ff4d78ed38091af02ed
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-08-03 15:55:24 +0530

    card loader for list completed

--- src/components/Loaders/CardLoader.jsx
+++ src/components/Loaders/CardLoader.jsx
@@ -8,6 +8,9 @@ const CardLoader = () => {
     ? 'bg-light-grey'
     : 'bg-white';
 
+  const themeTextStyle = isDarkTheme
+    ? 'bg-white'
+    : 'bg-primary-black';
   return (
     <div
       className={classNames(
@@ -15,7 +18,35 @@ const CardLoader = () => {
         themeBackgroundStyle
       )}
     >
-      hello
+      <div className="w-full h-[160px]"></div>
+      <div className="mt-6 ml-6">
+        <div
+          className={classNames(
+            themeTextStyle,
+            'w-[100px] h-5 rounded-full opacity-50'
+          )}
+        ></div>
+        <div className="flex flex-col gap-2 mt-4">
+          <div
+            className={classNames(
+              themeTextStyle,
+              'w-[90px] h-2 rounded-full opacity-50'
+            )}
+          ></div>
+          <div
+            className={classNames(
+              themeTextStyle,
+              'w-[90px] h-2 rounded-full opacity-50'
+            )}
+          ></div>
+          <div
+            className={classNames(
+              themeTextStyle,
+              'w-[90px] h-2 rounded-full opacity-50'
+            )}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Loaders/CardLoaders.jsx
+++ src/components/Loaders/CardLoaders.jsx
@@ -3,7 +3,7 @@ import CardLoader from './CardLoader';
 
 const CardLoaders = () => {
   return (
-    <div>
+    <div className="mx-auto px-4 flex flex-wrap items-center justify-center gap-y-10 gap-x-10 md:justify-start lg:gap-x-[74px] lg:gap-y-[72px] 2xl:px-0 ">
       <CardLoader />
       <CardLoader />
       <CardLoader />
